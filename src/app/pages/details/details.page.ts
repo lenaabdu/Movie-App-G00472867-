28d2e6ac2aaa103ee3f5fff9cfdfa07ac15d2422
lenaabdu <lenaabdu23@gmail.com>
2026-04-27 10:21:27 +0000

    Update movie service by adding person details API and update details page with actor information

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -35,12 +35,9 @@ import { home, arrowBack } from 'ionicons/icons';
   ]
 })
 export class DetailsPage {
-
-  person: any;
+  person: any; // Stores selected person 
   movies: any[] = [];
-
-  home = home;
-
+  home = home;// Icon reference for home button
   constructor(
     private route: ActivatedRoute,
     private movie: movie,
@@ -50,24 +47,26 @@ export class DetailsPage {
      }
 
   ngOnInit() {
+    // Get person ID from URL
     const id = Number(this.route.snapshot.paramMap.get('id'));
-
+  // Fetch person details from API
     this.movie.getDetails(id).subscribe((res: any) => {
       this.person = res;
     });
-    
+    // Fetch movies associated with the person
     this.movie.getDetailsMovies(id).subscribe((res: any) => {
       this.movies = res.cast.slice(0, 10);
     });
   }
-
+    // Navigate to home page
   goHome() {
     this.router.navigate(['/home']);
   }
-
+ // Navigate to selected movie details page
   openMovie(id: number) {
     this.router.navigate(['/movie-details', id]);
   }
+   // Go back to previous page
   goBack() {
     window.history.back();
   }
