c1e76170262c11865c581f5278a8a1ffa12cbadd
lenaabdu <lenaabdu23@gmail.com>
2026-04-27 10:01:00 +0000

    Update routing module: add details page route and improve code with comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,28 +2,33 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // Home page (default landing page)
   {
     path: 'home',
     loadComponent: () =>
       import('./home/home.page').then(m => m.HomePage)
   },
+    // Redirect empty path to home
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  // Movie Details page (uses movie ID parameter)
   {
     path: 'movie-details/:id',
     loadComponent: () =>
       import('./pages/movie-details/movie-details.page')
         .then(m => m.MovieDetailsPage)
   },
+  // Favourites page (displays saved movies)
   {
     path: 'favourites',
     loadComponent: () =>
       import('./pages/favourites/favourites.page')
         .then(m => m.FavouritesPage)
   },
+   // Details page (for cast/crew member information)
   {
     path: 'details/:id',
     loadComponent: () =>
